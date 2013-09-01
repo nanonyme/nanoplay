970cf99a7618ce35408c85ef7ea1a5b21d7b69ba
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2013-09-01 21:51:59 +0300

    Necessary magic to actually get the plugin included

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup
+import os
+
+def refresh_plugin_cache():
+    from twisted.plugin import IPlugin, getPlugins
+    list(getPlugins(IPlugin))
 
 setup(name='nanoplay',
       version='0.1',
       description='nanoplay is a naivistic music playing server',
       author='Seppo Yli-Olli',
       author_email='seppo.yliolli@gmail.com',
-      packages=['nanoplay'],
+      packages=['nanoplay', 'twisted.plugins'],
+      package_data={"twisted" : ["plugins/nanoplay_plugin.py"]},
+      requires=['Twisted'],
       scripts=['scripts/nanoplay']
      )
+
+refresh_plugin_cache()
