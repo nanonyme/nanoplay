b0c98897f3be41d83a4f32c5190db5e67bd23c42
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2013-09-01 20:11:29 +0300

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nanoplay',
+      version='0.1',
+      description='nanoplay is a naivistic music playing server',
+      author='Seppo Yli-Olli',
+      author_email='seppo.yliolli@gmail.com',
+      packages=['nanoplay'],
+     )
